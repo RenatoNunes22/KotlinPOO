bf69061ccddf00bbb5278aafdaf4bb2217eb81b7
Renato Nunes <renatonunes0011@hotmail.com>
2022-06-26 23:21:59 -0300

    incluido private set e inner class

--- src/main/kotlin/digonebank/Pessoa.kt
+++ src/main/kotlin/digonebank/Pessoa.kt
@@ -1,6 +1,18 @@
 package digonebank
 
 class Pessoa {
-    var nome : String = "Renato Nunes";
-    var cpf: String = "118.896.534-46";
+    var nome : String = "Renato Nunes"
+
+    var cpf: String = "118.896.534-46"
+    private set
+    inner class Endereco{
+        var rua : String = "Rua Teste"
+    }
+}
+
+fun main(){
+    val jether = Pessoa();
+    println(jether.nome);
+    println(jether.cpf);
+    println(jether.Endereco().rua);
 }
